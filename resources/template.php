3da6a040713e94103681edc1e475edfbe4e606de
Douglas Choi <douglchoi@gmail.com>
2013-02-03 13:33:36 -0800

    added base template with styles

---
+++ resources/template.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Template</title>
+	<meta name="description" content="Template">
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+<body>
+<div id="root_container">
+
+<!--- Site Header --->
+<header>
+	<div class="banner">Yo, this is a banner</div>
+	<input class="search_bar" type="text" value="Search..."/>
+</header>
+
+<!--- Navigation --->
+<nav>
+	<ul>	
+		<li><a href="#">Link 1</a></li>
+		<li><a href="#">Link 2</a></li>
+		<li><a href="#">Link 3</a></li>
+		<li><a href="#">Link 4</a></li>
+	</ul>
+</nav>
+
+<!--- Content --->
+<div id="main_content">
+
+</div>
+
+<!--- Footer --->
+<footer>
+	<ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Tools & Downloads</a></li>
+		<li><a href="#">Meet the Team</a></li>
+	</ul>
+	<p class="disclaimer">
+		<b>Disclaimer:</b> Lorem ipsum dolor sit amet, consectetur 
+		adipiscing elit. Integer dictum posuere orci at scelerisque. 
+		Maecenas sagittis tincidunt quam venenatis tristique. Morbi 
+		ac risus et ipsum molestie vulputate.
+	</p>
+</footer>
+
+</div>
+</body>
+</html>
